cd42a18ba0c75ba6334c3f41888f0026fd89bf14
Usaid Sharique <mohdu3106@gmail.com>
2025-07-28 17:35:34 +0530

    added projects

---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+export const projects = [
+    {
+        title:"Think",
+        imageSrc: "/assets/images/think/think.jpg",
+    },
+    {
+        title:"Spotify",
+        imageSrc: "/assets/images/spotify/spotify.jpg",
+    },
+    {
+        title:"Hearts And Quills",
+        imageSrc: "/assets/images/hearts-and-quills/hearts-and-quills-1.jpg",
+    },
+    {
+        title:"Netflix",
+        imageSrc: "/assets/images/netflix/netflix.jpg",
+    },
+]
